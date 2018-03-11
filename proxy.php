206894785622e8604fe553cbfebfbea9b279a656
Moritz Fromm <moritzgitfromm@gmail.com>
2018-03-11 19:16:33 +0100

    - Adding stacked bar icon (for menu)  - adding Setting modal  - Adding auto refresh

--- proxy.php
+++ proxy.php
@@ -18,27 +18,19 @@
  */
 
 error_reporting(E_ALL);
-
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 10.03.18
- * Time: 12:19
- */
-
 function startsWith($haystack, $needle)
 {
     return substr($haystack, 0, strlen($needle)) === $needle;
 }
 
 $class = "";
+$date = "";
 if (isset($_GET["class"])) {
     $class = "?class=" . $_GET["class"];
 }
 
 $url = "https://rsh.noah-seefried.de/v1.0/" . $class;
 $curl = curl_init($url);
-//https://www.joe0.com/2017/03/06/how-to-anonymize-traffic-programmatically-by-using-phpcurl-and-tor-network/
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 120);
 curl_setopt($curl, CURLOPT_MAXREDIRS, 20);
